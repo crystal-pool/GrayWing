72bce7c0281c5249d1eb9b7f9c86a0b272ef720d
forest93 <forest93@163.com>
2022-06-05 16:17:01 +0800

    Upgrade to Angular 11.

--- graywing-client/src/app/sparql-variable-binding-view/sparql-variable-binding-view.component.spec.ts
+++ graywing-client/src/app/sparql-variable-binding-view/sparql-variable-binding-view.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 
 import { SparqlVariableBindingViewComponent } from "./sparql-variable-binding-view.component";
 
@@ -6,7 +6,7 @@ describe("SparqlVariableBindingViewComponent", () => {
   let component: SparqlVariableBindingViewComponent;
   let fixture: ComponentFixture<SparqlVariableBindingViewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SparqlVariableBindingViewComponent ]
     })
--- graywing-client/src/app/status-indicator/status-indicator.component.spec.ts
+++ graywing-client/src/app/status-indicator/status-indicator.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 
 import { StatusIndicatorComponent } from "./status-indicator.component";
 
@@ -6,7 +6,7 @@ describe("StatusIndicatorComponent", () => {
   let component: StatusIndicatorComponent;
   let fixture: ComponentFixture<StatusIndicatorComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ StatusIndicatorComponent ]
     })
